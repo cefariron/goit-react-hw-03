e105e0de59109ed0e3592c46167c98dc7d0dbe2f
cefariron <adidaswww17@gmail.com>
2024-01-30 19:36:12 +0200

    complete wthout styles

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,13 +1,21 @@
-import css from '../Contact/Contact.module.css';
+import css from "../Contact/Contact.module.css";
+import { MdLocalPhone } from "react-icons/md";
+import { IoMdContact } from "react-icons/io";
 
 export const Contact = ({ id, name, number, onDelete }) => {
   return (
-      <div className={css.container}>
-        <div>
-          <p>{name}</p>
-          <p>{number}</p>
-        </div>
-        <button type="button" onClick={() => onDelete(id)}>Delete</button>
+    <div className={css.container}>
+      <div>
+        <p>
+          <MdLocalPhone  className={css.icon}/> {name}
+        </p>
+        <p>
+          <IoMdContact  className={css.icon}/> {number}
+        </p>
       </div>
-    )
-  };
+      <button type="button" onClick={() => onDelete(id)}>
+        Delete
+      </button>
+    </div>
+  );
+};
